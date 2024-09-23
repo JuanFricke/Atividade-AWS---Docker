1c363228e3a277e0e262060d4c83aaf073e52ada
juanfricke <juanpfricke@hotmail.com>
2024-09-23 16:30:36 -0300

    architecture changes

--- start-ec2.sh
+++ start-ec2.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y snapd git
+sudo apt-get install -y snapd git awscli
+# aws s3 cp s3://.env
 
 sudo snap install docker
 sudo usermod -aG docker $USER
 
 # Clona o repositório
 git clone https://github.com/JuanFricke/Atividade-AWS---Docker.git
-cd Atividade-AWS---Docker/wordpress
 
-docker build -t wordpress_image .
-docker run -d --name wordpress_container -p 8080:80 wordpress_image
+docker-compose up
 
 echo "O ambiente foi configurado com sucesso e o WordPress está rodando no Docker."
